795519c9793035cbda7fd581c6444813ada1abd0
geoffreyjiang <geoffreychjiang@gmail.com>
2022-02-10 20:18:03 -0800

    finshed 04 and 05

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -5,14 +5,25 @@ variable using the const keyword.
 
 Examples:
 
-arrowAvgValue([10, 20]); // => 15
-arrowAvgValue([2, 3, 7]); // => 4
-arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
 // your code here!
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+const arrowAvgValue = (arr) => {
+  let avg;
+  let sum = 0;
+  arr.forEach(function (el) {
+    sum += el;
+  });
+  avg = sum / arr.length;
+  return avg;
+};
+
+arrowAvgValue([10, 20]); // => 15
+arrowAvgValue([2, 3, 7]); // => 4
+arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 
 try {
   module.exports = arrowAvgValue;
--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -6,12 +6,27 @@ Write this function using an arrow function!
 
 Examples:
 
-arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
-arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
 // your code here
 
+let arrowMirrorArray = (arr) => {
+  let newArr = [];
+  let clone = arr.slice();
+  //console.log(clone);
+  arr.forEach(function (el) {
+    // let shift = clone.unshift(el);
+    // console.log(shift);
+    //let rev = arr.reverse();
+    //console.log(rev);
+    newArr.unshift(el);
+  });
+  return clone.concat(newArr);
+};
+console.log(arrowMirrorArray([1, 2, 3])); // => [ 1, 2, 3, 3, 2, 1 ]
+
+// arrowMirrorArray([1, 2, 3]); // => [ 1, 2, 3, 3, 2, 1 ]
+// arrowMirrorArray(["a", "b", "c", "d"]); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
