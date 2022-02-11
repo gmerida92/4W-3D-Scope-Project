d174aa07c4c5b055a9192a3618b4fd79a3428e4b
geoffreyjiang <geoffreychjiang@gmail.com>
2022-02-10 19:21:47 -0800

    finished questions 00 and 01

--- problems/00-arrow-addfive.js
+++ problems/00-arrow-addfive.js
@@ -8,15 +8,17 @@ ability to implicitly return by leaving out your own return statement.
 
 
 Examples:
-let result1 = addFive(0); // returns 5
-let result2 = addFive(10); // returns 15
-let result3 = addFive(37); // returns 42
 
 
 ***********************************************************************/
 
-let testVariable = 5 * 8;
+let addFive = (num) => num + 5;
 
+let result1 = addFive(0); // returns 5
+let result2 = addFive(10); // returns 15
+let result3 = addFive(37); // returns 42
+
+console.log(result1, result2, result3);
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = addFive;
